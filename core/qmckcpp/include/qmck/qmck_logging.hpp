a1d65fdd9eccc335e0c5fcdb9694b943a2e6bda2
Tom Kiesow <tomkiesow123@gmail.com>
2019-10-02 15:46:57 -0700

    add more logging macro delegations

--- core/qmckcpp/include/qmck/qmck_logging.hpp
+++ core/qmckcpp/include/qmck/qmck_logging.hpp
@@ -13,6 +13,8 @@
     #define QMCK_VLOG_IF_F(verbosity, cond, fmt, ...) VLOG_IF_F(verbosity, cond, fmt, __VA_ARGS__)
     #define QMCK_RAW_LOG_F(verbosity_name, fmt, ...) RAW_LOG_F(verbosity_name, fmt, __VA_ARGS__)
     #define QMCK_RAW_VLOG_F(verbosity, fmt, ...) RAW_VLOG_F(verbosity, fmt,__VA_ARGS__)
+    #define QMCK_LOG_SCOPE_F(verbosity_name, fmt, ...) LOG_SCOPE_F(verbosity_name, fmt, __VA_ARGS__)
+    #define QMCK_VLOG_SCOPE_F(verbosity, fmt, ...) VLOG_SCOPE_F(verbosity, fmt, __VA_ARGS__)
 #else
     #define QMCK_LOG_F(verbosity_name, ...) ;
     #define QMCK_VLOG_F(verbosity, ...) ;
@@ -20,5 +22,7 @@
     #define QMCK_VLOG_IF_F(verbosity, cond, fmt, ...) ;
     #define QMCK_RAW_LOG_F(verbosity_name, fmt, ...) ;
     #define QMCK_RAW_VLOG_F(verbosity, fmt, ...) ;
+    #define QMCK_LOG_SCOPE_F(verbosity_name, fmt, ...) ;
+    #define QMCK_VLOG_SCOPE_F(verbosity, fmt, ...) ;
 #endif
 
